75d0a8ec39f52320f319372acad01200b2f94a51
raghix123 <raghav.bodicherla@gmail.com>
2026-02-10 16:23:02 -0800

    Initial Commit

---
+++ ProximityUnlockMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProximityUnlockMac
+//
+//  Created by Raghav Bodicherla on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProximityUnlockMac/ProximityUnlockMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProximityUnlockMacApp.swift
+//  ProximityUnlockMac
+//
+//  Created by Raghav Bodicherla on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ProximityUnlockMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
